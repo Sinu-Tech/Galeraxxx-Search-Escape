7d6f85e4e5cfb65b9eb33926f22b4684a5d0aa71
Milena <milena20182018@gmail.com>
2022-08-19 22:51:33 -0300

    WIP

--- src/main.py
+++ src/main.py
@@ -3,20 +3,10 @@ from models.Enemy import Enemy
 
 
 if __name__ == "__main__":
-    user = Player("Player", 0, 0, 1.2)
+    user = Player("Player", 5, 10, 1.2)
 
-    user.move('down')
-    print(user.pos_x, user.pos_y)
-    user.move('up')
-    user.move('right')
-    print(user.pos_x, user.pos_y)
-    user.move('left')
+    enemy1 = Enemy("Enemy1", 10, 19, 1.0)
 
-    enemy1 = Enemy("Enemy1", 0, 0, 1.0)
-
-    enemy1.move('down')
-    print(enemy1.pos_x, enemy1.pos_y)
-    enemy1.move('up')
-    enemy1.move('right')
-    print(enemy1.pos_x, enemy1.pos_y)
-    enemy1.move('left')
+    enemy1.print_maph()
+    enemy1.update_maph(user.pos_x, user.pos_y)
+    enemy1.print_maph()
--- src/models/Enemy.py
+++ src/models/Enemy.py
@@ -1,7 +1,16 @@
 from .abstracts.AbstractCharcater import AbstractCharacter
-
+import numpy as np
 
 class Enemy(AbstractCharacter):
     def __init__(self, name:str, pos_x:int, pos_y:int, speed:float):
         super().__init__(name, pos_x, pos_y, speed)
+        self.map_h = np.array([[0]*20]*20)
+
+    def  update_maph(self, pos_xp:int, pos_yp:int):
+        for y in range(0, 20):
+            for x in range(0, 20):
+                self.map_h[y, x] = abs(pos_xp-x) + abs(pos_yp-y)
+
+    def print_maph(self):
+        print(self.map_h)
 
