67b0e5c42a91d594034a516c21b6d0b678c3b797
Ericles Dos Santos <ericles.santos@inventione.com.br>
2022-08-20 16:56:39 -0300

    [WIP] Print debugs

--- src/main.py
+++ src/main.py
@@ -2,19 +2,21 @@ from pprint import pprint
 from models import Cell, Enemy, Player
 
 if __name__ == "__main__":
-    user = Player("Player", 19, 19, 1.2)
+    user = Player("Player", 4,4, 1.2)
     enemy1 = Enemy("Enemy1", 0, 0, 1.0)
 
-    enemy1.update_map_fh(user.pos_x, user.pos_y)
-    # print("Mapa de f antes:")
-    # enemy1.print_maph()
+    # enemy1.update_map_fh(user.pos_x, user.pos_y)
 
     back = enemy1.get_expansions(user.pos_x, user.pos_y)
-    # print("Mapa de f depois:")
-    # enemy1.print_maph()
-    # print("Expansões:")
 
+    enemy1.print_map_f()
+    enemy1.print_map_g()
+    enemy1.print_map_h()
+    print("back:")    
+    print(back.x, back.y)
     while back != None:
         back = back.expanded_by
         if back != None:
             print(back.x, back.y)
+
+    enemy1.print_visiteds()
--- src/models/Enemy.py
+++ src/models/Enemy.py
@@ -1,16 +1,15 @@
-from cmath import exp
 import numpy as np
 from .abs_class.AbstractModel import AbstractCharacter
 from .Cell import *
 from .enums.EnumMap import *
 
-map_1 = np.array([[3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-                  [0, 1, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+map_1 = np.array([[3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
                   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,99 +24,135 @@ map_1 = np.array([[3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                   [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
                   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
+map_2 = np.array([[3, 0, 0, 0, 0],
+                  [0, 1, 0, 1, 0],
+                  [0, 0, 0, 1, 1],
+                  [0, 1, 0, 1, 0],
+                  [1, 1, 0, 0, 0]])
+print(map_2)
+
 
 def better_index(expansions: list):
     better_index = 0
     better_f = expansions[0].f
     for index, e in enumerate(expansions):
-        if e.f < better_f:
+        if e.f <= better_f:
             better_f = e.f
             better_index = index
     return better_index
+
+
+def get_euclidean_distance(x_aux: int, y_aux: int, pos_xp: int, pos_yp: int):
+    # return ((pos_xp - x_aux) ** 2 + (pos_yp - y_aux) ** 2) ** (1 / 2)
+    return abs(pos_xp - x_aux) + abs(pos_yp - y_aux)
 
 
 class Enemy(AbstractCharacter):
     def __init__(self, name: str, pos_x: int, pos_y: int, speed: float):
         super().__init__(name, pos_x, pos_y, speed)
         self.map_h = []
-        for x in range(0, 20):
+        for y in range(0, 5):
             temp = []
-            for y in range(0, 20):
+            for x in range(0, 5):
                 temp.append(Cell(x, y, 0, 0, 0, False, None))
             self.map_h.append(temp)
+        self.map_h = np.array(self.map_h)
+        print(self.map_h)
         self.expansions = []
 
-    def update_map_fh(self, pos_xp: int, pos_yp: int):
-        for y in range(0, 20):
-            for x in range(0, 20):
-                self.map_h[y][x].h = abs(pos_xp - x) + abs(pos_yp - y)
-
-    def print_maph(self):
-        print("f")
-        for i in range(0, 20):
-            for j in range(0, 20):
-                print(self.map_h[i][j].f, end=" ")
+    
+    def print_visiteds(self):
+        for x in self.map_h:
+            for y in x:
+                if y.was_visited:
+                    print(1, end=" ")
+                else:
+                    print(0, end=" ")
             print("")
-        print("g")
-        for i in range(0, 20):
-            for j in range(0, 20):
+
+    def print_map_h(self):
+        print("Mapa heurístico")
+        for i in range(0, 5):
+            for j in range(0, 5):
+                print("{:.0f}".format(self.map_h[i][j].h), end=" ")
+            print("")
+
+    def print_map_g(self):
+        print("Mapa da função g")
+        for i in range(0, 5):
+            for j in range(0, 5):
                 print(self.map_h[i][j].g, end=" ")
             print("")
-        print("h")
-        for i in range(0, 20):
-            for j in range(0, 20):
-                print(self.map_h[i][j].h, end=" ")
-            print("")
 
-    def print_expansions(self):
-        for e in self.expansions:
-            print(e.x, e.y, e.f, e.g, e.h)
+    def print_map_f(self):
+        print("Mapa da função F")
+        for i in range(0, 5):
+            for j in range(0, 5):
+                print("{:.0f}".format(self.map_h[i, j].f), end=" ")
+            print("")
 
     def get_expansions(self, pos_xp: int, pos_yp: int):
         self.expansions.append(self.map_h[self.pos_x][self.pos_y])
         x_aux, y_aux = self.expansions[0].x, self.expansions[0].y
         index = better_index(self.expansions)
 
-        while len(self.expansions) > 0 and (self.expansions[index].x != pos_xp or self.expansions[index].y != pos_yp):
-
+        while len(self.expansions) > 0:
+            if self.expansions[index].x == pos_xp and self.expansions[index].y == pos_yp:
+                return self.expansions[index]
             # down
-            if x_aux < 19 and map_1[x_aux + 1][y_aux] == MAP_FREE and self.map_h[x_aux + 1][y_aux].was_visited == False:
-                map_1[x_aux + 1][y_aux] = -1
+            if x_aux < 4 and map_2[x_aux + 1][y_aux] == MAP_FREE and self.map_h[x_aux + 1][y_aux].was_visited == False:
+                print("down")
+                map_2[x_aux + 1][y_aux] = -1
                 g = self.map_h[x_aux][y_aux].g + 1
-                h = self.map_h[x_aux][y_aux].h
+                h = get_euclidean_distance(x_aux + 1, y_aux, pos_xp, pos_yp)
                 f = g + h
-                self.map_h[x_aux+1][y_aux].g = g
-                self.map_h[x_aux+1][y_aux].f = f
+                self.map_h[x_aux + 1][y_aux].g = g
+                self.map_h[x_aux + 1][y_aux].h = h
+                self.map_h[x_aux + 1][y_aux].f = f
+                self.map_h[x_aux + 1][y_aux].was_visited = True
+                self.print_visiteds()
                 self.expansions.append(
                     Cell(x_aux + 1, y_aux, f, g, h, True, self.expansions[index]))
             # up
-            if x_aux > 0 and map_1[x_aux - 1][y_aux] == MAP_FREE and self.map_h[x_aux - 1][y_aux].was_visited == False:
-                map_1[x_aux - 1][y_aux] = -1
+            if x_aux > 0 and map_2[x_aux - 1][y_aux] == MAP_FREE and self.map_h[x_aux - 1][y_aux].was_visited == False:
+                print("up")
+                map_2[x_aux - 1][y_aux] = -1
                 g = self.map_h[x_aux][y_aux].g + 1
-                self.map_h[x_aux-1][y_aux].g = g
-                h = self.map_h[x_aux-1][y_aux].h
+                h = get_euclidean_distance(x_aux - 1, y_aux, pos_xp, pos_yp)
                 f = g + h
-                self.map_h[x_aux-1][y_aux].f = f
+                self.map_h[x_aux - 1][y_aux].h = h
+                self.map_h[x_aux - 1][y_aux].g = g
+                self.map_h[x_aux - 1][y_aux].f = f
+                self.map_h[x_aux - 1][y_aux].was_visited = True
+                self.print_visiteds()
                 self.expansions.append(
                     Cell(x_aux - 1, y_aux, f, g, h, True, self.expansions[index]))
             # right
-            if y_aux < 19 and map_1[x_aux][y_aux + 1] == MAP_FREE and self.map_h[x_aux ][y_aux+1].was_visited == False:
-                map_1[x_aux][y_aux + 1] = -1
+            if y_aux < 4 and map_2[x_aux][y_aux + 1] == MAP_FREE and self.map_h[x_aux][y_aux + 1].was_visited == False:
+                print("right")
+                map_2[x_aux][y_aux + 1] = -1
                 g = self.map_h[x_aux][y_aux].g + 1
-                self.map_h[x_aux][y_aux+1].g = g
-                h = self.map_h[x_aux][y_aux+1].h
+                h = get_euclidean_distance(x_aux, y_aux + 1, pos_xp, pos_yp)
                 f = g + h
+                self.map_h[x_aux][y_aux + 1].h = h
+                self.map_h[x_aux][y_aux + 1].g = g
                 self.map_h[x_aux][y_aux + 1].f = f
+                self.map_h[x_aux][y_aux + 1].was_visited = True
+                self.print_visiteds()
                 self.expansions.append(
                     Cell(x_aux, y_aux + 1, f, g, h, True, self.expansions[index]))
-            # lef
-            if y_aux > 0 and map_1[x_aux][y_aux - 1] == MAP_FREE and self.map_h[x_aux][y_aux-1].was_visited == False:
-                map_1[x_aux][y_aux - 1] = -1
+            # left
+            if y_aux > 0 and map_2[x_aux][y_aux - 1] == MAP_FREE and self.map_h[x_aux][y_aux - 1].was_visited == False:
+                print("left")
+                map_2[x_aux][y_aux - 1] = -1
                 g = self.map_h[x_aux][y_aux].g + 1
-                self.map_h[x_aux][y_aux+1].g = g
-                h = self.map_h[x_aux][y_aux-1].h
+                h = get_euclidean_distance(x_aux, y_aux - 1, pos_xp, pos_yp)
                 f = g + h
+                self.map_h[x_aux][y_aux - 1].h = h
+                self.map_h[x_aux][y_aux - 1].g = g
                 self.map_h[x_aux][y_aux - 1].f = f
+                self.map_h[x_aux][y_aux - 1].was_visited = True
+                self.print_visiteds()
                 self.expansions.append(
                     Cell(x_aux, y_aux + 1, f, g, h, True, self.expansions[index]))
 
@@ -125,6 +160,3 @@ class Enemy(AbstractCharacter):
             if len(self.expansions) > 0:
                 index = better_index(self.expansions)
                 x_aux, y_aux = self.expansions[index].x, self.expansions[index].y
-                if self.expansions[index].x == pos_xp and self.expansions[index].y == pos_yp:
-                    return self.expansions[index]
-
