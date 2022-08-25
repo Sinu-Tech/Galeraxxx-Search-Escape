38e57eadf899f47f7c3c0b9269dd4b10b27dfbe2
Joanne Stephany PC <joannestephany@gmail.com>
2022-08-24 22:24:51 -0300

    Add: Movimento continuo player

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from models import Enemy, Player
 from utils.map_utils import *
 from models.maps import *
+import keyboard 
+
 if __name__ == "__main__":
 
     user = Player("Player", 4, 4, 1.2)
@@ -19,3 +21,27 @@ if __name__ == "__main__":
     # print_map_f(enemy1.map_bot)
     # print_map_g(enemy1.map_bot)
     # print_map_h(enemy1.map_bot)
+
+
+    while True:                                                                 
+        pressedKey = keyboard.read_key()
+        if pressedKey == "a":
+            print("clicou em a")
+            if(user.pos_x >0):
+                print('move a')
+            
+        if pressedKey == "d":
+            print("clicou em d")
+            if(user.pos_x >width_map):
+                print('move d')
+            
+        if pressedKey == "w":
+            print("clicou em w")
+            if(user.pos_y >0):
+                print('move w')
+            
+        if pressedKey == "s":
+            print("clicou em s")
+            if(user.pos_y >height_map):
+                print('move s')
+                
